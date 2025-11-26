5cf49008e5f7c8b4e9ce297bd28218808b9c373c
jhonioliver <ojhonioliver@gmail.com>
2025-11-26 18:27:10 -0300

    downsell

--- context/layer-provider.tsx
+++ context/layer-provider.tsx
@@ -30,8 +30,8 @@ export function LayerProvider({
 }: LayerProviderProps) {
 
   // SET LINKS
-  const frontLink = 'https://pay.hotmart.com/';
-  const promoLink = 'https://pay.hotmart.com/';
+  const frontLink = 'https://go.centerpag.com/PPU38CQ3Q0B';
+  const promoLink = 'https://go.centerpag.com/PPU38CQ3U0O';
 
   const contextValue = {
     host,
